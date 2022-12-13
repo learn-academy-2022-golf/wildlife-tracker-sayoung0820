bd68fcff5241059036768d0ede668c69f68a4455
sayoung0820 <shannonashley78@gmail.com>
2022-12-13 17:05:57 -0500

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  resources :animals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+GET    /animals(.:format)                                                                                animals#index
+POST   /animals(.:format)                                                                                animals#create
+GET    /animals/new(.:format)                                                                            animals#new
+GET    /animals/:id/edit(.:format)                                                                       animals#edit
+GET    /animals/:id(.:format)                                                                            animals#show
+PATCH  /animals/:id(.:format)                                                                            animals#update
+PUT    /animals/:id(.:format)                                                                            animals#update
+DELETE /animals/:id(.:format)  
+ 
+end   
