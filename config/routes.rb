7452cae6a1b5cae1f5021a75eb9ca206ad2d4c19
sayoung0820 <shannonashley78@gmail.com>
2022-12-13 18:02:42 -0500

    first story completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-GET    /animals(.:format)                                                                                animals#index
-POST   /animals(.:format)                                                                                animals#create
-GET    /animals/new(.:format)                                                                            animals#new
-GET    /animals/:id/edit(.:format)                                                                       animals#edit
-GET    /animals/:id(.:format)                                                                            animals#show
-PATCH  /animals/:id(.:format)                                                                            animals#update
-PUT    /animals/:id(.:format)                                                                            animals#update
-DELETE /animals/:id(.:format)  
+  # root "articles#index" 
+
  
 end   
